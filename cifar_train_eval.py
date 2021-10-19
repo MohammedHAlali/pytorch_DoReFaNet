f26c6fd8c0a7d53c5b101fb3f80e0c628b5865f7
MohammedHAlali <alali@huskers.unl.edu>
2021-10-19 15:50:24 -0500

    create out folder

--- cifar_train_eval.py
+++ cifar_train_eval.py
@@ -19,14 +19,14 @@ from utils.preprocessing import *
 # Training settings
 parser = argparse.ArgumentParser(description='DoReFa-Net pytorch implementation')
 
-parser.add_argument('--root_dir', type=str, default='./')
+parser.add_argument('--root_dir', type=str, default='out/')
 parser.add_argument('--data_dir', type=str, default='../data')
 parser.add_argument('--log_name', type=str, default='resnet_w1a32')
 parser.add_argument('--pretrain', action='store_true', default=False)
 parser.add_argument('--pretrain_dir', type=str, default='./ckpt/resnet20_baseline')
 
 parser.add_argument('--cifar', type=int, default=10)
-
+parser.add_argument('--exp_id', type=int, default=0) #logging
 parser.add_argument('--Wbits', type=int, default=1)
 parser.add_argument('--Abits', type=int, default=32)
 
@@ -44,16 +44,27 @@ parser.add_argument('--num_workers', type=int, default=1)
 parser.add_argument('--cluster', action='store_true', default=False)
 
 cfg = parser.parse_args()
-
-cfg.log_dir = os.path.join(cfg.root_dir, 'logs', cfg.log_name)
-cfg.ckpt_dir = os.path.join(cfg.root_dir, 'ckpt', cfg.log_name)
-
-os.makedirs(cfg.log_dir, exist_ok=True)
-os.makedirs(cfg.ckpt_dir, exist_ok=True)
+print('arguments: ', cfg)
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"] = cfg.use_gpu
 
+out_path = os.path.join('out', str(cfg.exp_id))
+if(os.path.exists(out_path)):
+    print(out_path, ' exists')
+    cfg.exp_id += 1
+    out_path = os.path.join('out', str(cfg.exp_id))
+    print('new out_path: ', out_path)
+if(not os.path.exists(out_path)):
+    print(out_path, ' not exists')
+    os.mkdir(out_path)
+
+cfg.log_dir = os.path.join(cfg.root_dir, str(cfg.exp_id), 'logs', cfg.log_name)
+cfg.ckpt_dir = os.path.join(cfg.root_dir, str(cfg.exp_id), 'ckpt', cfg.log_name)
+print('log dir: ', cfg.log_dir)
+
+os.makedirs(cfg.log_dir, exist_ok=True)
+os.makedirs(cfg.ckpt_dir, exist_ok=True)
 
 def main():
   if cfg.cifar == 10:
